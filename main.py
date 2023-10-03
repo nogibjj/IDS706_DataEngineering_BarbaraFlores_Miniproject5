21e33e2b497348f7d98fc23c009dd32cc681d9bb
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-10-03 06:10:13 +0000

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ ETL-Query script
 """
 
 from mylib.extract import extract
-#from mylib.transform_load import load
+from mylib.transform_load import load
 #from mylib.query import query
 
 # Extract
@@ -11,8 +11,8 @@ print("Extracting data...")
 extract()
 
 # Transform and load
-#print("Transforming data...")
-#load()
+print("Transforming data...")
+load()
 
 # Query
 #print("Querying data...")
