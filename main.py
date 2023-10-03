750d06e859711f3b37f2a5abe59a11cfe329ce5f
Barbara Flores Rios <143648839+BarbaraPFloresRios@users.noreply.github.com>
2023-10-03 05:49:33 +0000

    update

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ ETL-Query script
 """
 
 from mylib.extract import extract
-from mylib.transform_load import load
-from mylib.query import query
+#from mylib.transform_load import load
+#from mylib.query import query
 
 # Extract
 print("Extracting data...")
 extract()
 
 # Transform and load
-print("Transforming data...")
-load()
+#print("Transforming data...")
+#load()
 
 # Query
-print("Querying data...")
-query()
+#print("Querying data...")
+#query()
--- mylib/extract.py
+++ mylib/extract.py
@@ -5,11 +5,13 @@ food dataset
 """
 import requests
 
-def extract(url="https://raw.githubusercontent.com/Barabasi-Lab/GroceryDB/main/data/GroceryDB_IgFPro.csv", 
-            file_path="data/GroceryDB_IgFPro.csv"):
+def extract(url="https://github.com/nickeubank/practicaldatascience/blob/master/Example_Data/world-small.csv", 
+            file_path="data/world-small.csv"):
     """"Extract a url to a file path"""
     timeout = 100
     with requests.get(url, timeout=timeout) as r:
         with open(file_path, 'wb') as f:
             f.write(r.content)
     return file_path
+
+extract()
